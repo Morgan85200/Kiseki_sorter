b50e80a551254fd6ab4f7d5a7a9ec475b439a114
Morgan Pousson <pousson.morgan85@gmail.com>
2024-08-07 18:10:51 +0200

    fc complete sc near complete

--- src/js/data/2024-07-20.js
+++ src/js/data/2024-07-20.js
@@ -23,7 +23,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Why is my present a BOY ?",
     img: "https://i.ibb.co/G3v0dFT/present-boy.jpg",
-    url: "https://youtu.be/UZDiiTP3OYE?si=d7vpzdzxfp_yfZ6W&t=1068",
+    url: "https://youtu.be/KuU6G92UtaM?si=ZFD0JZEAnYGghKS5&t=89",
     opts: {
       arcs: ["skyFC"]
     }
@@ -31,7 +31,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Joshua ends his dream with Estelle",
     img: "https://i.ibb.co/MCdn4zg/joshua-dream.jpg",
-    url: "https://youtu.be/UZDiiTP3OYE?si=d7vpzdzxfp_yfZ6W&t=1068",
+    url: "https://youtu.be/Jo6rH6Hx-ng?si=mnxvcyW5h6jdJYII&t=3529",
     opts: {
       arcs: ["skyFC"]
     }
@@ -39,7 +39,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Picnic in Manoria Village",
     img: "https://i.ibb.co/RSQcVcB/picnic-in-manoria.jpg",
-    url: "https://youtu.be/UZDiiTP3OYE?si=d7vpzdzxfp_yfZ6W&t=1068",
+    url: "https://youtu.be/EvAF4kYs6V4?si=t3ahwYfAGKbeLtLT&t=32",
     opts: {
       arcs: ["skyFC"]
     }
@@ -47,15 +47,15 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Professor Russel rescue attempt failed",
     img: "https://i.ibb.co/w6bMQN5/tita-rescue-attempt.jpg",
-    url: "https://youtu.be/UZDiiTP3OYE?si=d7vpzdzxfp_yfZ6W&t=1068",
+    url: "https://youtu.be/lOaSSYQcaCg?si=ZyevM44AdN2l_Q_d&t=747",
     opts: {
       arcs: ["skyFC"]
     }
   },
   {
-    name: "Mayor Delmore chase and arrest",
+    name: "Joshua snapping, Mayor Delmore chase and arrest",
     img: "https://i.ibb.co/xG45000/joshua-snaping.jpg",
-    url: "https://youtu.be/UZDiiTP3OYE?si=d7vpzdzxfp_yfZ6W&t=1068",
+    url: "https://youtu.be/Leu1KiAPBPw?si=la4LUtM7qTc2H9Hy&t=2704",
     opts: {
       arcs: ["skyFC"]
     }
@@ -63,6 +63,23 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Stop being such a child, Richard!",
     img: "https://i.ibb.co/Yk3cmPZ/cassius-speech-to-richard.jpg",
+    url: "https://youtu.be/5RGxP0cFBxE?si=A4O__I0re0SBZzzQ&t=3707",
+    opts: {
+      arcs: ["skyFC"]
+    }
+  },
+  {
+    name: "Olivier recounts how he got arrested",
+    img: "https://i.ibb.co/Bcq3fTz/olivier-arrested.jpg",
+    url: "https://youtu.be/oG9WvG50ASQ?si=n-I5fymtme3tUdDW&t=756",
+    opts: {
+      arcs: ["skyFC"]
+    }
+  },
+  {
+    name: "Estelle gives comfort to Kloe after the orphanage burnt down",
+    img: "https://i.ibb.co/DRRYBXg/estelle-conforts-kloe.jpg",
+    url: "https://youtu.be/fdOKxCVmUs8?si=JTiYIzv7plaqGkJ6&t=2073",
     opts: {
       arcs: ["skyFC"]
     }
@@ -70,6 +87,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Jenis Royal Academy Play",
     img: "https://i.ibb.co/ZhH25Mv/jenis-academy-play.jpg",
+    url: "https://youtu.be/ndyd8dvTBmE?si=nT8x8cpsP-JFoZzi&t=1161",
     opts: {
       arcs: ["skyFC"]
     }
@@ -77,6 +95,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "The Tournament Arc",
     img: "https://i.ibb.co/ZTjtG2j/tournament-arc.jpg",
+    url: "https://youtu.be/c-TnoRPHA1o?si=eKo-Rb45yM7-DCvY&t=637",
     opts: {
       arcs: ["skyFC"]
     }
@@ -84,6 +103,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "First time meeting Olivier",
     img: "https://i.ibb.co/309QD8B/Olivier-s-first-meeting.jpg",
+    url: "https://youtu.be/R1hWpQrmmrY?si=5s44lKkmHSm3Y6Nz&t=1071",
     opts: {
       arcs: ["skyFC"]
     }
@@ -91,6 +111,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Joshua plays harmonica for Estelle",
     img: "https://i.ibb.co/hR6KvcF/Joshua-plays-for-estelle.jpg",
+    url: "https://youtu.be/wlR6qDcLjTw?si=nZ_-AIAhT_e6_uYx&t=1048",
     opts: {
       arcs: ["skyFC"]
     }
@@ -98,6 +119,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Infiltrating the Capua Sky Bandits",
     img: "https://i.ibb.co/MGS7F75/infiltrating-capua-family.jpg",
+    url: "https://youtu.be/UFdxm6RuPFk?si=Rf0FliO2QR6tcUPa&t=191",
     opts: {
       arcs: ["skyFC"]
     }
@@ -105,6 +127,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Kloe confronts Gilbert for burning down the orphanage",
     img: "https://i.ibb.co/yqMSCLR/kloe-confronting-gilbert.jpg",
+    url:"https://youtu.be/Leu1KiAPBPw?si=9AUeSwba8mCuEByJ&t=862",
     opts: {
       arcs: ["skyFC"]
     }
@@ -112,13 +135,15 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Agate fights Lorence for the first time",
     img: "https://i.ibb.co/djDC1sK/agate-fights-lorence.jpg",
+    url:"https://youtu.be/-wHWKwR5UoQ?si=Kix3crotNUVO5qhg&t=95",
     opts: {
       arcs: ["skyFC"]
     }
   },
   {
-    name: "Estelle declares herself Tita's sister",
+    name: "Estelle declares herself Tita's big sister",
     img: "https://i.ibb.co/bQBsy8J/estelle-tita-sister.jpg",
+    url:"https://youtu.be/Lfd5DCuzpDc?si=s-mZQPKKJ4PHSquV&t=1453",
     opts: {
       arcs: ["skyFC"]
     }
@@ -126,6 +151,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Agate hallucinating about his sister",
     img: "https://i.ibb.co/W5LFSMT/agate-hallucinating-mischa.jpg",
+    url: "https://youtu.be/lOaSSYQcaCg?si=i64r_qopkOttV6b_&t=2432",
     opts: {
       arcs: ["skyFC"]
     }
@@ -133,6 +159,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Infiltrating Leiston Fortress",
     img: "https://i.ibb.co/4fwDt26/infiltrating-leiston-fortress.jpg",
+    url: "https://youtu.be/r-rYGYjqVRA?si=PLcCl8t9JXv977Yv&t=844",
     opts: {
       arcs: ["skyFC"]
     }
@@ -140,6 +167,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Estelle tells Joshua his past",
     img: "https://i.ibb.co/dk7RN85/estelle-past.png",
+    url: "https://youtu.be/aVde7GlXOlc?si=uJeJ12t7C7BehvLn&t=573",
     opts: {
       arcs: ["skyFC"]
     }
@@ -147,6 +175,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Estelle and Joshua promise to one another",
     img: "https://i.ibb.co/YXQg3My/I-will-tell-you-everything.jpg",
+    url: "https://youtu.be/58X-LSlXzGE?si=RShk3Luwx2SQdjHy&t=1355",
     opts: {
       arcs: ["skyFC"]
     }
@@ -154,6 +183,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Estelle and Joshua disguises as maids",
     img: "https://i.ibb.co/mDs0F8T/joshua-estelle-disguising-as-maids.jpg",
+    url: "https://youtu.be/sYz7zGjY680?si=MknRQ83wLNpiXjxR&t=816",
     opts: {
       arcs: ["skyFC"]
     }
@@ -161,13 +191,15 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Hostage liberation, Kloe is the princess !? ",
     img: "https://i.ibb.co/z88jqZ2/saving-the-princess.jpg",
+    url: "https://youtu.be/5Rj8wFxOY1U?si=_iB0FMhw6fnTzTDj&t=667",
     opts: {
       arcs: ["skyFC"]
     }
   },
   {
-    name: "Fight against Lorence",
+    name: "Estelle, Kloe and Scherazard confront Lorence",
     img: "https://i.ibb.co/QHLnxWK/first-fight-against-lorence.jpg",
+    url: "https://youtu.be/5RGxP0cFBxE?si=XsGiKJDSrkgDl82I&t=169",
     opts: {
       arcs: ["skyFC"]
     }
@@ -175,6 +207,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Now! finish him off !",
     img: "https://i.ibb.co/FBVv8Cj/now-finish-him-off.jpg",
+    url: "https://youtu.be/5RGxP0cFBxE?si=NTNpUpZplgzAialm&t=3232",
     opts: {
       arcs: ["skyFC"]
     }
@@ -182,6 +215,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Estelle and Joshua become bracers",
     img: "https://i.ibb.co/9bMBvtk/estelle-joshua-becomes-bracers.jpg",
+    url: "https://youtu.be/Jo6rH6Hx-ng?si=pHpJPi8BrBSdLglz&t=66",
     opts: {
       arcs: ["skyFC"]
     }
@@ -189,20 +223,15 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Weissman's reveal",
     img: "https://i.ibb.co/CsCK0dh/weissman-reveal-and-joshua-past.jpg",
+    url: "https://youtu.be/W7InC56YLYw?si=smvL6WRZmBTAaCTm&t=188",
     opts: {
       arcs: ["skyFC"]
     }
   },
   {
-    name: "First chapter's ending",
-    img: "https://i.ibb.co/5M49pd0/fc-ending.jpg",
-    opts: {
-      arcs: ["skyFC"]
-    }
-  },
-  {
-    name: "The end of a journey, the beginning of a new one", // SC ending 
-    img: "https://i.ibb.co/5M49pd0/fc-ending.jpg",
+    name: "The end of a journey, the beginning of a new one",
+    img: "https://i.ibb.co/1LL6WgB/sc-ending.jpg",
+    url: "https://youtu.be/VUGHNBWND58?si=wyZ_qH8vBpFsnUVx&t=3403",
     opts: {
       arcs: ["skySC"]
     }
@@ -210,6 +239,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Kevin helps Estelle vent her feelings",
     img: "https://i.ibb.co/mG5cW7N/kevin-helps-estelle.jpg",
+    url: "https://youtu.be/0UnpPEmQnLc?si=S0qqq2kv22CzI1ie&t=686",
     opts: {
       arcs: ["skySC"]
     }
@@ -217,6 +247,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Estelle finds her resolve once again",
     img: "https://i.ibb.co/1K2rSG2/estelle-resolve.jpg",
+    url: "https://youtu.be/0UnpPEmQnLc?si=dwvl_VTcj_r8-gSK&t=1653",
     opts: {
       arcs: ["skySC"]
     }
@@ -224,6 +255,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Meeting Renne for the first time",
     img: "https://i.ibb.co/gMZPZRx/meeting-renne.jpg",
+    url: "https://youtu.be/wrxVNrotOSI?si=AxNaMKD0dW8j34LL&t=1405",
     opts: {
       arcs: ["skySC"]
     }
@@ -231,6 +263,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Estelle and Kloe open up to each other",
     img: "https://i.ibb.co/ZShk2pB/estelle-kloe-open-up-to-each-others.jpg",
+    url: "https://youtu.be/uD-KJ0O-vTw?si=QvuVe_GHINAKxBqv&t=649",
     opts: {
       arcs: ["skySC"]
     }
@@ -238,6 +271,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Bleublanc and Olivier debates about beauty",
     img: "https://i.ibb.co/vBCGgjL/true-beauty-is-love.jpg",
+    url: "https://youtu.be/uD-KJ0O-vTw?si=SYSAceQuxC5DUFA5&t=3277",
     opts: {
       arcs: ["skySC"]
     }
@@ -245,6 +279,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Walter shows himself",
     img: "https://i.ibb.co/HrdDyCM/walter-shows-himself.jpg",
+    url: "https://youtu.be/8azI-9re0G8?si=ei5gOYRc3103pJMk&t=1992",
     opts: {
       arcs: ["skySC"]
     }
@@ -252,6 +287,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Olivier cheers up Estelle",
     img: "https://i.ibb.co/WKgGG7m/olivier-cheers-up-estelle.jpg",
+    url: "https://youtu.be/c7OUMTD6oGc?si=0b3EllosadXCXbIX&t=519",
     opts: {
       arcs: ["skySC"]
     }
@@ -259,6 +295,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Olivier saves the day with his gentle, sad song...",
     img: "https://i.ibb.co/6RPGW5s/olivier-saving-the-day.jpg",
+    url: "https://youtu.be/wrxVNrotOSI?si=lutRPTtjXnFKEWls&t=2084",
     opts: {
       arcs: ["skySC"]
     }
@@ -266,6 +303,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Zin saves Estelle from Walter",
     img: "https://i.ibb.co/47FRnf4/zin-saves-estelle.jpg",
+    url: "https://youtu.be/8azI-9re0G8?si=TaexopwjHdwBE_R5&t=2399",
     opts: {
       arcs: ["skySC"]
     }
@@ -273,6 +311,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Renne plays hide and seek with the bracers",
     img: "https://i.ibb.co/FWqmcR8/renne-plays-hide-and-seek.jpg",
+    url: "https://youtu.be/nIjMlGSwbEE?si=_9x1x8VS0Vqi9GQO&t=791",
     opts: {
       arcs: ["skySC"]
     }
@@ -280,6 +319,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Renne reveals her true nature",
     img: "https://i.ibb.co/JnsJPBy/renne-reveals-her-true-nature.jpg",
+    url: "https://youtu.be/F2FprU5lF9I?si=gRa1QF3QqpGjrAVE&t=1315",
     opts: {
       arcs: ["skySC"]
     }
@@ -287,6 +327,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Richard disbands the intelligence division",
     img: "https://i.ibb.co/7twt2rY/richard-disbands-the-intelligence-division.jpg",
+    url: "https://youtu.be/F2FprU5lF9I?si=GY9Qahr-Q3jzVquw&t=1935",
     opts: {
       arcs: ["skySC"]
     }
@@ -294,6 +335,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Josette and Joshua face off Mueller to get back the Bobcat",
     img: "https://i.ibb.co/7WWcYQG/joshua-face-muller.jpg",
+    url: "https://youtu.be/UWnJse-805Y?si=97OKqFbsQTpL7e-b&t=1100",
     opts: {
       arcs: ["skySC"]
     }
@@ -301,6 +343,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Estelle places her trust in Olivier",
     img: "https://i.ibb.co/xgLmZky/estelle-trust-olivier.jpg",
+    url: "https://youtu.be/UWnJse-805Y?si=LfbJ_Th_lmrDBF1j&t=1665",
     opts: {
       arcs: ["skySC"]
     }
@@ -308,6 +351,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Scherazard reminisce about how she met the Bright Family",
     img: "https://i.ibb.co/fGKCtmQ/schera-meeting-bright-family.jpg",
+    url: "https://youtu.be/-r_Af30KvNY?si=q4y-7EOxJ_dSJyP7&t=1201",
     opts: {
       arcs: ["skySC"]
     }
@@ -315,6 +359,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Schera slaps Estelle and reminds her that everyone make mistakes",
     img: "https://i.ibb.co/pJP766f/schera-slaps-estelle.jpg",
+    url: "https://youtu.be/HJKxkAelK1o?si=ik1Wx1oJaH5fmlfF&t=377",
     opts: {
       arcs: ["skySC"]
     }
@@ -322,13 +367,47 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Estelle plays the Whereabouts of Light and wakes up from her dream",
     img: "https://i.ibb.co/PFL95Rn/estelle-wakes-up.png",
+    url: "https://youtu.be/HJKxkAelK1o?si=7DLjd_xK6qZMBnwG&t=2706",
     opts: {
       arcs: ["skySC"]
     }
   },
   {
-    name: "Estelle plays the Whereabouts of Light and wakes up from her dream",
-    img: "https://i.ibb.co/PFL95Rn/estelle-wakes-up.png",
+    name: "Anton confesses to Aina, they end up doing a drinking contest with Olivier and Schera",
+    img: "https://i.ibb.co/02xfTMj/anton-confesses-aina.jpg",
+    url: "https://youtu.be/UZDiiTP3OYE?si=dvkVS-eUxIgaccYh&t=976",
+    opts: {
+      arcs: ["skySC"]
+    }
+  },
+  {
+    name: "Loewe tells Agate what separates them",
+    img: "https://i.ibb.co/5kR5tN4/loewe-speech-agate.jpg",
+    url: "https://youtu.be/VQh9bJnBW3o?si=qTTN0Wl_d4HVFM_E&t=1436",
+    opts: {
+      arcs: ["skySC"]
+    }
+  },
+  {
+    name: "Tita rushes in to protect Agate",
+    img: "https://i.ibb.co/3vJ5Ndb/tita-protects-agate.jpg",
+    url: "https://youtu.be/VQh9bJnBW3o?si=hSC7kDJL82HIMYBu&t=1634",
+    opts: {
+      arcs: ["skySC"]
+    }
+  },
+  {
+    name: "Anton confesses to Aina, they end up doing a drinking contest with Olivier and Schera",
+    img: "https://i.ibb.co/02xfTMj/anton-confesses-aina.jpg",
+    url: "https://youtu.be/UZDiiTP3OYE?si=dvkVS-eUxIgaccYh&t=976",
+    opts: {
+      arcs: ["skySC"]
+    }
+  },
+  {
+    name: "Anton confesses to Aina, they end up doing a drinking contest with Olivier and Schera",
+    img: "https://i.ibb.co/02xfTMj/anton-confesses-aina.jpg",
+    url: "https://youtu.be/UZDiiTP3OYE?si=dvkVS-eUxIgaccYh&t=976",
     opts: {
       arcs: ["skySC"]
     }
